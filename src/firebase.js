f4571ee986b253cd5bcdedfb856c70247e32716f
Marin Perak <marin.perak7@gmail.com>
2022-11-07 16:29:30 +0100

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBMTfdFXPVYreT72PirhMVkRw7XC9yWtDY",
+  authDomain: "clone-2fcda.firebaseapp.com",
+  projectId: "clone-2fcda",
+  storageBucket: "clone-2fcda.appspot.com",
+  messagingSenderId: "241155536380",
+  appId: "1:241155536380:web:465cbaedcead514a662e79",
+  measurementId: "G-XMFBD67HGV",
+};
