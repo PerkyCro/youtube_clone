c763892cf53c179518c69f8a2b5eb3dfc12311c3
Marin Perak <marin.perak7@gmail.com>
2022-11-22 23:02:51 +0100

    2:22

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -7,14 +7,86 @@ function RecommendedVideos() {
     <div className="recommendedVideos">
       <h2>Recommended</h2>
       <div className="recommendedVideos__videos">
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
+        <VideoCard
+          title="Best of MMA"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/ytc/AMLnZu-l1Bdjrw0N8TSDjNsaMgt6JO9fFfSASj9dHA=s176-c-k-c0x00ffffff-no-rj"
+          channel="PerkyCro"
+          image="https://i.ytimg.com/vi/V3_OMp-8Pio/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBKHdq5NUPvSZxuTK9CVoznF6AC5g"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes / 2019/2020"
+          views="2.1M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/ytc/AMLnZu-l1Bdjrw0N8TSDjNsaMgt6JO9fFfSASj9dHA=s176-c-k-c0x00ffffff-no-rj"
+          channel="PerkyCro"
+          image="https://i.ytimg.com/an_webp/lkIFF4maKMU/mqdefault_6s.webp?du=3000&sqp=CO7u9JsG&rs=AOn4CLAieXe9Qx-L5whSTpNANmjIu7Xqvg"
+        />
+        <VideoCard
+          title="Two Maine Coon Cats on Adventure"
+          views="22M Views"
+          timestamp="5 days ago"
+          channelImage="https://yt3.ggpht.com/ytc/AMLnZu-l1Bdjrw0N8TSDjNsaMgt6JO9fFfSASj9dHA=s176-c-k-c0x00ffffff-no-rj"
+          channel="Maine Coon world"
+          image="https://i.ytimg.com/vi/r7Fbh-p0YfI/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDCtE9VS6dIKkGo-54c8pVEB3O2yg"
+        />
+        <VideoCard
+          title="Let 3 i TBF feat. El Combo - Izgubljeni"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/ytc/AMLnZu-l1Bdjrw0N8TSDjNsaMgt6JO9fFfSASj9dHA=s176-c-k-c0x00ffffff-no-rj"
+          channel="Let 3"
+          image="https://i.ytimg.com/an_webp/xEbddxgsA4Y/mqdefault_6s.webp?du=3000&sqp=CNzi9JsG&rs=AOn4CLDCpjNhL7g48Y2LcE1hFxynHkjHng"
+        />
+        <VideoCard
+          title="Music"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/ytc/AMLnZu-l1Bdjrw0N8TSDjNsaMgt6JO9fFfSASj9dHA=s176-c-k-c0x00ffffff-no-rj"
+          channel="WebDJ"
+          image="https://i.ytimg.com/vi/PfQ7BPDDe6Y/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCwNTGW8_PSKTUARK4deU-Ovq0wHA"
+        />
+        <VideoCard
+          title="Best of MMA"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/ytc/AMLnZu-l1Bdjrw0N8TSDjNsaMgt6JO9fFfSASj9dHA=s176-c-k-c0x00ffffff-no-rj"
+          channel="PerkyCro"
+          image="https://i.ytimg.com/vi/V3_OMp-8Pio/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBKHdq5NUPvSZxuTK9CVoznF6AC5g"
+        />
+        <VideoCard
+          title="Become a Web Developer in 10 minutes / 2019/2020"
+          views="2.1M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/ytc/AMLnZu-l1Bdjrw0N8TSDjNsaMgt6JO9fFfSASj9dHA=s176-c-k-c0x00ffffff-no-rj"
+          channel="PerkyCro"
+          image="https://i.ytimg.com/an_webp/lkIFF4maKMU/mqdefault_6s.webp?du=3000&sqp=CO7u9JsG&rs=AOn4CLAieXe9Qx-L5whSTpNANmjIu7Xqvg"
+        />
+        <VideoCard
+          title="Two Maine Coon Cats on Adventure"
+          views="22M Views"
+          timestamp="5 days ago"
+          channelImage="https://yt3.ggpht.com/ytc/AMLnZu-l1Bdjrw0N8TSDjNsaMgt6JO9fFfSASj9dHA=s176-c-k-c0x00ffffff-no-rj"
+          channel="Maine Coon world"
+          image="https://i.ytimg.com/vi/r7Fbh-p0YfI/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDCtE9VS6dIKkGo-54c8pVEB3O2yg"
+        />
+        <VideoCard
+          title="Let 3 i TBF feat. El Combo - Izgubljeni"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/ytc/AMLnZu-l1Bdjrw0N8TSDjNsaMgt6JO9fFfSASj9dHA=s176-c-k-c0x00ffffff-no-rj"
+          channel="Let 3"
+          image="https://i.ytimg.com/an_webp/xEbddxgsA4Y/mqdefault_6s.webp?du=3000&sqp=CNzi9JsG&rs=AOn4CLDCpjNhL7g48Y2LcE1hFxynHkjHng"
+        />
+        <VideoCard
+          title="Music"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://yt3.ggpht.com/ytc/AMLnZu-l1Bdjrw0N8TSDjNsaMgt6JO9fFfSASj9dHA=s176-c-k-c0x00ffffff-no-rj"
+          channel="WebDJ"
+          image="https://i.ytimg.com/vi/PfQ7BPDDe6Y/hqdefault.jpg?sqp=-oaymwEXCOADEI4CSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLCwNTGW8_PSKTUARK4deU-Ovq0wHA"
+        />
       </div>
     </div>
   );
--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,17 +1,18 @@
 import { Avatar } from "@mui/material";
 import React from "react";
+import "./VideoCard.css";
 
-function VideoCard({ image, title, channel, views, timestamp }) {
+function VideoCard({ image, title, channel, views, timestamp, channelImage }) {
   return (
     <div className="videoCard">
-      <img src={image} alt=""></img>
+      <img className="videoCard__thumbnail" src={image} alt=""></img>
       <div className="videoCard__info">
         <Avatar className="videoCard__avatar" alt={channel} src={channel} />
-        <div className="video__text">
+        <div className="videoCard__text">
           <h4>{title}</h4>
           <p>{channel}</p>
           <p>
-            {views} {timestamp}
+            {views} • {timestamp}
           </p>
         </div>
       </div>
